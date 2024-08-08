161ec5e14140c122e86228d80469291968d69b85
Abbas <abbas.r23@gmail.com>
2024-08-08 11:38:45 +0530

    added mui

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
+import { Button } from '@mui/material'
+import Dashboard from './dashboard/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://electron-vite.github.io" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -21,13 +23,14 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+       
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Button variant="contained">Hello world</Button> */}
+      <Dashboard/>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,4 +17,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 // Use contextBridge
 window.ipcRenderer.on('main-process-message', (_event, message) => {
   console.log(message)
-})
+}) 
